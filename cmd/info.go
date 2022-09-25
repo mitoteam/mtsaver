ca83a88cc8ec2ff8c8a22eaca7565a0fd6817f35
Mikhail Oleynik <mishutka@mito-team.com>
2022-09-25 19:55:29 +0300

    7-zip path autodetection

--- cmd/info.go
+++ cmd/info.go
@@ -13,7 +13,8 @@ var CmdInfo = cli.Command{
 	Action: func(ctx *cli.Context) error {
 		fmt.Println(mtsaver.Global.AppName + " version: " + mtsaver.Global.Version)
 		fmt.Println("Built with: " + mtsaver.Global.BuiltWith)
-		fmt.Println("7-zip Command: " + mtsaver.Global.SevenZipCmd)
+		fmt.Println("7-zip command: " + mtsaver.Global.SevenZipCmd)
+		fmt.Println("7-zip info: " + mtsaver.Global.SevenZipInfo)
 		return nil
 	},
 }
--- mtsaver/global.go
+++ mtsaver/global.go
@@ -1,16 +1,22 @@
 package mtsaver
 
 import (
+	"bufio"
+	"log"
+	"os"
+	"os/exec"
 	"runtime"
+	"strings"
 
 	"github.com/urfave/cli/v2"
 )
 
 var Global struct {
-	AppName     string
-	Version     string
-	BuiltWith   string
-	SevenZipCmd string
+	AppName      string
+	Version      string
+	BuiltWith    string
+	SevenZipCmd  string
+	SevenZipInfo string
 }
 
 func init() {
@@ -22,10 +28,61 @@ func init() {
 func SetupBeforeCommand(ctx *cli.Context) error {
 	Global.SevenZipCmd = ctx.String("7zip")
 
-	if len(Global.SevenZipCmd) == 0 || Global.SevenZipCmd == "auto" {
+	if Global.SevenZipCmd == "auto" {
+		Global.SevenZipCmd = "" //reset to force autodetection
+	}
+
+	if len(Global.SevenZipCmd) > 0 {
+		if !checkSevenZipCommand(Global.SevenZipCmd) {
+			log.Fatalln("Can not run provided 7-Zip command: " + Global.SevenZipCmd)
+		}
+	} else {
 		//try autodetect
-		Global.SevenZipCmd = "ha ha auto"
+		//try raw 7z command
+		r := checkSevenZipCommand("7z")
+
+		if !r {
+			switch runtime.GOOS {
+			case "windows":
+				r = checkSevenZipCommand(os.Getenv("ProgramFiles") + "\\7-Zip\\7z.exe")
+
+				if !r {
+					r = checkSevenZipCommand(os.Getenv("ProgramFiles(x86)") + "\\7-Zip\\7z.exe")
+				}
+			case "linux":
+				r = checkSevenZipCommand("/usr/lib/p7zip/7z")
+				if !r {
+					r = checkSevenZipCommand("/usr/bin/7z")
+				}
+				if !r {
+					r = checkSevenZipCommand("/bin/7z")
+				}
+			}
+		}
+
+		if !r {
+			log.Fatalln("Can not find 7-Zip. Please provide correct path with --7zip flag.")
+		}
 	}
 
 	return nil
+}
+
+func checkSevenZipCommand(cmd string) bool {
+	out, err := exec.Command(cmd).Output()
+
+	if err != nil {
+		return false
+	}
+
+	scanner := bufio.NewScanner(strings.NewReader(string(out)))
+	for scanner.Scan() {
+		if scanner.Text() != "" {
+			Global.SevenZipInfo = scanner.Text()
+			Global.SevenZipCmd = cmd
+			return true
+		}
+	}
+
+	return false
 }
